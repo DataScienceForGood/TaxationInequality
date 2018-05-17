7d54ff28236603ca292d679eba33cf9f84cf240f
Reinhold Koch <reinhold.koch@roche.com>
2018-05-17 20:09:57 +0200

    read in taxes broken down by quintiles

--- taxation.R
+++ taxation.R
@@ -14,3 +14,28 @@ ggpairs(data = dat,
 
 ggplot(dat, aes(x=praemienverbilligungsquote, y=sozialhilfequote, color=wohnviertel)) + 
   geom_point()
+
+
+einkommen <- readxl::read_excel('data/Steuern_Klassen_Wohnviertel_plus_Quintile.xlsx', skip = 8) %>% 
+  slice(1:105) %>% 
+  select(1:6) %>% 
+  fill(X__1, Wohnviertel) %>% 
+  rename(wohnviertelid = X__1)
+
+vermoegen <-  readxl::read_excel('data/Steuern_Klassen_Wohnviertel_plus_Quintile.xlsx', skip = 122) %>% 
+  slice(1:105) %>% 
+  select(1:6) %>% 
+  fill(X__1, X__2) %>% 
+  rename(wohnviertelid = X__1, Wohnviertel = X__2)
+
+einkommen2 <- readxl::read_excel('data/Steuern_Klassen_Wohnviertel_plus_Quintile.xlsx', skip = 8) %>% 
+  slice(1:105) %>% 
+  select(8:13) %>% 
+  fill(X__3, X__4) %>% 
+  rename(wohnviertelid = X__3, Wohnviertel = X__4, einkommenquintile = X__5)
+
+vermoegen2 <- readxl::read_excel('data/Steuern_Klassen_Wohnviertel_plus_Quintile.xlsx', skip = 122) %>% 
+  slice(1:105) %>% 
+  select(8:13) %>% 
+  fill(X__4, X__5) %>% 
+  rename(wohnviertelid = X__4, Wohnviertel = X__5, vermoegensquintile = X__6)
